e37a2c4d0e905aa19125e533fd915865eaa9dc11
rehanmemon007 <rehanmemon1920@yahoo.com>
2024-06-01 20:35:12 +0500

    update

--- src/Components/Stepper/Form/Form1.jsx
+++ src/Components/Stepper/Form/Form1.jsx
@@ -3,36 +3,123 @@ import { Button } from "@mui/material";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from 'yup'
 import TextField from '@mui/material/TextField';
+import { auth } from "../../../Config/firebase";
+import firebase from "../../../Config/firebase";
+import { ToastContainer, toast } from 'react-toastify';
 
 function Form1(props) {
     console.log(props)
 
+    const signout=()=>{
+        firebase.auth().signOut().then(() => {
+            toast("account log out")
+            // Sign-out successful.
+          }).catch((error) => {
+            // An error happened.
+          });
+    }
+
+    const googleSignin =async () => {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase
+        .auth()
+        .signInWithPopup(provider)
+            .then((result) => {
+
+                var credential = result.credential;
+                var token = credential.accessToken;
+                var user = result.user;
+                console.log(user)
+
+            }).catch((error) => {
+
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                var email = error.email;
+
+                var credential = error.credential;
+                console.log(error)
+
+            });
+    }
+
+
+    const createAccount = (email, password) => {
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Signed in 
+                var user = userCredential.user;
+                console.log(user)
+                toast("account create", {
+                    position: "top-center",
+                    autoClose: 5018,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                   
+                });
+                // ...
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorMessage)
+                toast(errorMessage);
+
+                // ..
+            });
+
+
+    }
 
     return (
         <>
             <div style={{ width: 600 + "px", margin: "auto" }}>
                 <h1>Form 1</h1>
+                <ToastContainer
+                    position="top-center"
+                    autoClose={5018}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+
+                />
 
                 <Formik
-                    initialValues={{ firstName: "", email: "" }}
+                    initialValues={{ email: "", password: "" }}
                     validationSchema={Yup.object({
-                        firstName: Yup.string().min(5, "Enter Must Name greater then 5 ").max(10, "only enter less then 10 letter")
+                        password: Yup.string().min(5, "Enter Must Name greater then 5 ").max(10, "only enter less then 10 letter")
                             .required("FILED REQUIRED"),
                         email: Yup.string().email("Invalid Email").required("M,essage")
                     })}
                     onSubmit={(value) => {
-                        console.log("ok")
+                        console.log(value.email, value.password)
+                        createAccount(value.email, value.password)
                     }}
                 >
                     {props => (
                         <form onSubmit={props.handleSubmit}>
-                            <Field placeholder="test" type="text" name="firstName" />
                             <TextField id="outlined-basic" label="Outlined" variant="outlined"
-                                value={props.values.firstName}
+                                value={props.values.email}
                                 onChange={props.handleChange}
-                                name="firstName"
+                                name="email"
                             />
-                            <ErrorMessage name="firstName" />
+                            <ErrorMessage name="email" />
+                            <TextField id="outlined-basic" label="Outlined" variant="outlined"
+                                value={props.values.password}
+                                onChange={props.handleChange}
+                                name="password"
+                            />
+                            <ErrorMessage name="password" />
 
                             <button type="submit">Submit</button>
 
@@ -42,6 +129,10 @@ function Form1(props) {
 
 
                 </Formik>
+                <button onClick={()=>signout()}>Google signout</button>
+           
+                <button onClick={()=>googleSignin()}>Google signin</button>
+
 
 
 
---
+++ src/Config/firebase.jsx
@@ -0,0 +1,19 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/database";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBkvRj66JKpqauZpIl5qHOmsDBt-WSjJxk",
+    authDomain: "booming-voice-411517.firebaseapp.com",
+    databaseURL: "https://booming-voice-411517-default-rtdb.firebaseio.com",
+    projectId: "booming-voice-411517",
+    storageBucket: "booming-voice-411517.appspot.com",
+    messagingSenderId: "11647279445",
+    appId: "1:11647279445:web:065a7f23f560b338f10fdc"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  export const auth = firebase.auth();
+  export default firebase
+  
